8e1bfaebb13a7a88f94b2073675f41c04de3097c
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-10 14:34:24 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,29 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import 'providers/user_provider.dart';
 import 'providers/cart_provider.dart';
 import 'providers/orders_provider.dart';
-import 'providers/user_provider.dart';
 import 'modules/home/home_page.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+
+  // Load environment variables
   await dotenv.load(fileName: ".env");
+
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: dotenv.env['API_KEY']!,
+      authDomain: dotenv.env['AUTH_DOMAIN'],
+      projectId: dotenv.env['PROJECT_ID'],
+      storageBucket: dotenv.env['STORAGE_BUCKET'],
+      messagingSenderId: dotenv.env['MESSAGING_SENDER_ID'],
+      appId: dotenv.env['APP_ID'],
+    ),
+  );
+
   runApp(UserApp());
 }
 
