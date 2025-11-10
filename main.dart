a35079328eb48eee51411476a8d296bf79c4a937
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-10 12:43:41 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'providers/cart_provider.dart';
+import 'providers/orders_provider.dart';
+import 'providers/user_provider.dart';
+import 'modules/home/home_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -7,6 +14,25 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+  await dotenv.load(fileName: ".env");
+  runApp(UserApp());
 }
 
+class UserApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+        ChangeNotifierProvider(create: (_) => CartProvider()),
+        ChangeNotifierProvider(create: (_) => OrdersProvider()),
+      ],
+      child: MaterialApp(
+        title: 'User App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: HomePage(),
+      ),
+    );
+  }
+}
