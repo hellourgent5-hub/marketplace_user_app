d5a5389b8b952102f244208ba631b2fbe807c63a
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-10 12:53:24 +0530

    Update orders_provider.dart

--- lib/providers/orders_provider.dart
+++ lib/providers/orders_provider.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class OrdersProvider extends ChangeNotifier {
+  final CollectionReference ordersCollection =
+      FirebaseFirestore.instance.collection('orders');
+
+  Future<void> placeOrder(Map<String, dynamic> order) async {
+    await ordersCollection.add(order);
+  }
+}
 
