b992a595673dd22d493235f0c20122ee5e0e9380
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-10 12:53:55 +0530

    Update user_provider.dart

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class UserProvider extends ChangeNotifier {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  User? user;
+
+  Future<void> signIn(String email, String password) async {
+    final res = await _auth.signInWithEmailAndPassword(email: email, password: password);
+    user = res.user;
+    notifyListeners();
+  }
+
+  Future<void> signUp(String email, String password) async {
+    final res = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+    user = res.user;
+    notifyListeners();
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+    user = null;
+    notifyListeners();
+  }
+}
 
